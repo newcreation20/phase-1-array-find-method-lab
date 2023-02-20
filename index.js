98ba18ce3d94299b09392b2712b5998e775e84b5
Kerny <kerny14@gmail.com>
2023-02-20 12:13:51 -0500

    commit tree d44fb7db2672561f1372d843b4bb31e9eee4f6cb

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function superbowlWin(record) {
+	const win = record.find(obj => obj.result == "W");
+	return typeof win === 'undefined' ? undefined : win.year;
+}
+
+console.log(superbowlWin(record));
+
+
+
+
+// function superbowlWin(collection){
+//     for (const record of collection){
+//         if (record.result === "W"){
+//             console.log(result)
+//         }
+//     }
+    
+// }
+
 
